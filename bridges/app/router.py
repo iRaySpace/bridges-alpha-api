d6cda3314c7acb1e9041f1bc45459ed163b86f5a
irayspace <irayspacii@gmail.com>
2023-04-30 01:47:24 +0800

    feat: added transaction

--- bridges/app/router.py
+++ bridges/app/router.py
@@ -1,5 +1,5 @@
 from fastapi.routing import APIRouter
-from bridges.app.schema import Message
+from bridges.app.schema import Message, Transaction
 
 api_router = APIRouter()
 
@@ -7,3 +7,8 @@ api_router = APIRouter()
 @api_router.post('/', response_model=Message)
 async def send_echo_message(incoming_message: Message) -> Message:
     return incoming_message
+
+
+@api_router.post('/transaction')
+async def process_transaction(transaction: Transaction) -> None:
+    return None
--- bridges/app/schema.py
+++ bridges/app/schema.py
@@ -1,4 +1,12 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+
 
 class Message(BaseModel):
     message: str
+
+
+# TODO: add date
+class Transaction(BaseModel):
+    amount: int
+    source_uuid: str = Field(alias='sourceUuid')
+    destination_uuid: str = Field(alias='destinationUuid')
