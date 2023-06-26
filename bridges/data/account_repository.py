5842721d6fcf80be086b36710ccc6112e0926440
irayspace <irayspacii@gmail.com>
2023-06-26 14:36:00 +0800

    fix(account_repository): added Account type

--- bridges/data/account_repository.py
+++ bridges/data/account_repository.py
@@ -5,7 +5,7 @@ from bridges.domain.dto import AccountCreate
 data = []
 
 
-def create(account: AccountCreate):
+def create(account: AccountCreate) -> Account:
     account_no = randint(1000_0000_0000, 9999_9999_9999)
     new_account = {**account.dict(), 'accountNo': account_no}
     parsed_account = Account(**new_account)
