e63481524bbf15657f17599f57e36e5a1ed48f41
irayspace <irayspacii@gmail.com>
2023-06-26 14:38:54 +0800

    fix(boot): prefix

--- bridges/app/boot.py
+++ bridges/app/boot.py
@@ -4,5 +4,5 @@ from bridges.app.router import api_router
 
 def get_app() -> FastAPI:
     app = FastAPI()
-    app.include_router(router=api_router, prefix='/api')
+    app.include_router(router=api_router)
     return app
